08081f00c2e8ee61b95545a9292a2550ed0da7c1
ElhamFadel <elham2000Fadel@gmail.com>
2021-08-25 15:42:00 +0300

    convert sign sync to async

--- server/controllers/sign-in.js
+++ server/controllers/sign-in.js
@@ -1,8 +1,14 @@
-const { sign } = require('jsonwebtoken');
+const {
+  sign,
+} = require('jsonwebtoken');
 const {
   getUserData,
 } = require('../database/queries');
-const { comparePassword, hashPassword, signinValiadtion } = require('../utils');
+const {
+  comparePassword,
+  hashPassword,
+  signinValiadtion,
+} = require('../utils');
 
 module.exports = (req, res) => {
   const {
@@ -14,17 +20,28 @@ module.exports = (req, res) => {
     rows,
   }) => {
     if (!rows.length) {
-      res.status(302).json({ message: "Waaaa , your haven't any email" });
+      res.status(302).json({
+        message: " your haven't any email",
+      });
     } else {
-      const { name, password: hashPass, email } = rows[0];
+      const {
+        name,
+        password: hashPass,
+      } = rows[0];
       comparePassword(password, hashPass, (err, isMatchPass) => {
-        console.log(isMatchPass, hashPass);
         if (isMatchPass) {
-          /// async with using promise sign
-          const token = sign({ name, email }, process.env.secretKey);
-          res.cookie('access_token', token, { httpOnly: true }).status(200).redirect('/');
+          sign({
+            name,
+            email,
+          }, process.env.secretKey, (err, token) => {
+            res.cookie('access_token', token, {
+              httpOnly: true,
+            }).status(200).redirect('/');
+          });
         } else {
-          res.status(302).json({ message: "You're Password Is wrong" });
+          res.status(302).json({
+            message: "You're Password Is wrong",
+          });
         }
       });
     }
