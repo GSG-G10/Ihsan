23edef9a16bd73ccd18411ef471e25aba9509d28
rawand1998 <rawandgaradh@gmail.com>
2021-08-25 12:58:35 +0300

    sign in valiadtion relates #21

--- server/controllers/sign-in.js
+++ server/controllers/sign-in.js
@@ -2,34 +2,34 @@ const { sign } = require('jsonwebtoken');
 const {
   getUserData,
 } = require('../database/queries');
-const {
-  comparePassword,
-  hashPassword,
-} = require('../utils');
+const { comparePassword, hashPassword ,signinValiadtion } = require('../utils');
 
 module.exports = (req, res) => {
   const {
     email,
     password,
   } = req.body;
-
-  getUserData(email).then(({
-    rows,
-  }) => {
-    if (!rows.length) {
-      res.status(302).json({ message: "Waaaa , your haven't any email" });
-    } else {
-      const { name, password: hashPass, email } = rows[0];
-
-      comparePassword(password, hashPass, (err, isMatchPass) => {
-        console.log(isMatchPass, hashPass);
-        if (isMatchPass) {
-          const token = sign({ name, email }, process.env.secretKey);
-          res.cookie('access_token', token, { httpOnly: true }).status(200).redirect('/');
-        } else {
-          res.status(302).json({ message: "You're Password Is wrong" });
-        }
-      });
-    }
-  });
+  const { error, value } = signinValiadtion.validate({ email, password });
+  if (error) {
+    res.status(400).json({ msg: error.message });
+  } else {
+    getUserData(email).then(({
+      rows,
+    }) => {
+      if (!rows.length) {
+        res.status(302).json({ message: "Waaaa , your haven't any email" });
+      } else {
+        const { name, password: hashPass, email } = rows[0];
+        comparePassword(password, hashPass, (err, isMatchPass) => {
+          console.log(isMatchPass, hashPass);
+          if (isMatchPass) {
+            const token = sign({ name, email }, process.env.secretKey);
+            res.cookie('access_token', token, { httpOnly: true }).status(200).redirect('/');
+          } else {
+            res.status(302).json({ message: "You're Password Is wrong" });
+          }
+        });
+      }
+    });
+  }
 };
