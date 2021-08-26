7034511a0baf0a393b84940e07a0b9ab2b58edd1
ElhamFadel <elham2000Fadel@gmail.com>
2021-08-26 11:50:57 +0300

    client validation #37

--- public/scripts/dom.js
+++ public/scripts/dom.js
@@ -1,5 +1,7 @@
 const navBar = document.querySelector('.nav-bar');
-
+const password = document.querySelector('#password');
+const passError = document.querySelector('#passError');
+const form = document.querySelector('form');
 const createNavBar = (title) => {
   navBar.textContent = '';
   const logOut = document.createElement('a');
@@ -12,3 +14,12 @@ const createNavBar = (title) => {
   navBar.appendChild(logOut);
   navBar.appendChild(username);
 };
+form.addEventListener('submit', (event) => {
+  if (password.value.length < 6) {
+    event.preventDefault();
+    passError.textContent = 'PASSWORD SHOULD BE 6 CHARACTERS AT LEAST';
+    password.style.border = '2px solid red';
+  } else {
+    password.style.border = '1px solid #827e7e';
+  }
+});
--- server/database/connection.js
+++ server/database/connection.js
@@ -18,9 +18,7 @@ switch (NODE_ENV) {
 }
 const options = {
   connectionString: dbUrl,
-  ssl: {
-    rejectUnauthorized: false,
-  },
+  ssl: false,
 };
 
 module.exports = new Pool(options);
